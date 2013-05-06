58ef1f913e9322ee606b1bcded4db0e500079404
Fedor Indutny <fedor.indutny@gmail.com>
2013-05-06 21:35:56 +0400

    rewrite

--- src/linearscan.rs
+++ src/linearscan.rs
@@ -1,128 +1,150 @@
-pub struct Config {
-  register_count: uint
+type BlockId = uint;
+type InstrId = uint;
+type IntervalId = uint;
+type RegisterId = uint;
+type StackId = uint;
+
+pub struct GraphBuilder<K> {
+  block_id: BlockId,
+  instr_id: InstrId,
+  interval_id: IntervalId,
+  blocks: ~[@mut Block<K>]
+}
+
+pub struct BlockBuilder<'self, K> {
+  graph: &'self mut GraphBuilder<K>,
+  block: @mut Block<K>
+}
+
+struct Block<K> {
+  id: BlockId,
+  instructions: ~[@Instruction<K>],
+  successors: ~[@mut Block<K>],
+  predecessors: ~[@mut Block<K>]
+}
+
+struct Instruction<K> {
+  id: InstrId,
+  kind: InstrKind<K>,
+  output: @Interval,
+  inputs: ~[@Interval]
+}
+
+// Abstraction to allow having user-specified instruction types
+// as well as internal movement instructions
+enum InstrKind<K> {
+  User(K),
+  Move
+}
+
+struct Interval {
+  id: IntervalId,
+  value: Value,
+  ranges: ~[LiveRange],
+  parent: Option<@Interval>,
+  children: ~[@Interval]
+}
+
+enum Value {
+  Virtual,
+  Register(RegisterId),
+  Stack(StackId)
 }
 
 struct LiveRange {
-  start: uint,
-  end: uint
+  start: InstrId,
+  end: InstrId
 }
 
-enum UseKind {
-  UseAny,
-  UseRegister
+pub impl<K> GraphBuilder<K> {
+  fn new() -> ~GraphBuilder<K> {
+    return ~GraphBuilder {
+      block_id: 0,
+      instr_id: 0,
+      interval_id: 0,
+      blocks: ~[]
+    };
+  }
+
+  fn block(&mut self, body: &fn(b: &mut BlockBuilder<K>)) {
+    let b = Block::new(self);
+    self.blocks.push(b);
+    BlockBuilder::exec(self, b, body);
+  }
+
+  priv fn block_id(&mut self) -> BlockId {
+    let r = self.block_id;
+    self.block_id += 1;
+    return r;
+  }
+
+  priv fn instr_id(&mut self) -> InstrId {
+    let r = self.instr_id;
+    self.instr_id += 2;
+    return r;
+  }
+
+  priv fn interval_id(&mut self) -> IntervalId {
+    let r = self.interval_id;
+    self.interval_id += 1;
+    return r;
+  }
 }
 
-struct Use {
-  pos: uint,
-  kind: UseKind
+pub impl<'self, K> BlockBuilder<'self, K> {
+  priv fn exec(graph: &mut GraphBuilder<K>,
+               block: @mut Block<K>,
+               body: &fn(b: &mut BlockBuilder<K>)) {
+    let mut b = BlockBuilder {
+      graph: graph,
+      block: block
+    };
+    body(&mut b);
+  }
+
+  fn add(&'self self, kind: K, args: ~[@Instruction<K>]) -> @Instruction<K> {
+    let r = Instruction::new(self.graph, User(kind), args);
+    self.block.instructions.push(r);
+    return r;
+  }
+
+  fn end(&self) {
+  }
 }
 
-pub enum Value {
-  Virtual,
-  Register(uint),
-  Stack(uint)
+pub impl<K> Block<K> {
+  fn new(graph: &mut GraphBuilder<K>) -> @mut Block<K> {
+    return @mut Block {
+      id: graph.block_id(),
+      instructions: ~[],
+      successors: ~[],
+      predecessors: ~[]
+    };
+  }
 }
 
-pub struct Interval {
-  ranges: ~[LiveRange],
-  parent: Option<@Interval>,
-  children: ~[@Interval],
-  value: ~Value
+pub impl<K> Instruction<K> {
+  fn new(graph: &mut GraphBuilder<K>, kind: InstrKind<K>, args: ~[@Instruction<K>])
+      -> @Instruction<K> {
+    return @Instruction {
+      id: graph.instr_id(),
+      kind: kind,
+      output: Interval::new(graph),
+      inputs: do vec::map(args) |arg| {
+        arg.output
+      }
+    };
+  }
 }
 
-impl Interval {
-  pub fn new() -> @Interval {
+pub impl<K> Interval {
+  fn new(graph: &mut GraphBuilder<K>) -> @Interval {
     return @Interval {
+      id: graph.interval_id(),
+      value: Virtual,
       ranges: ~[],
       parent: None,
-      children: ~[],
-      value: ~Virtual
-    }
-  }
-}
-
-pub trait Block<I:Instruction> {
-  fn instruction_count(&self) -> uint;
-  fn instruction_at(&self, i: uint) -> @I;
-  fn predecessor_count(&self) -> uint;
-  fn predecessor_at(&self, i: uint) -> @Self;
-  fn successor_count(&self) -> uint;
-  fn successort_at(&self, i: uint) -> @Self;
-
-  fn insert_move(&self, pos: uint, from: @Interval, to: @Interval);
-}
-
-pub trait Instruction {
-  fn id(&self) -> uint;
-  fn output(&self) -> @Interval;
-  fn input_count(&self) -> uint;
-  fn input_at(&self, i: uint) -> @Interval;
-  fn is_move(&self) -> bool;
-}
-
-priv trait BlockHelper<I:Instruction> {
-  fn start(&self) -> uint;
-  fn end(&self) -> uint;
-  fn each_instr(&self, f: &fn(&I) -> bool);
-}
-
-impl<I:Instruction, B:Block<I> > BlockHelper<I> for B {
-  fn start(&self) -> uint {
-    assert!(self.instruction_count() >= 1);
-    return self.instruction_at(0).id();
-  }
-
-  fn end(&self) -> uint {
-    assert!(self.instruction_count() >= 1);
-    return self.instruction_at(self.instruction_count() - 1).id();
-  }
-
-  fn each_instr(&self, f: &fn(&I) -> bool) {
-    let n = self.instruction_count();
-    let mut i = 0;
-    while i < n {
-      if !f(self.instruction_at(i)) {
-        break;
-      }
-      i += 1;
-    }
-  }
-}
-
-priv struct BlockWrapper<B> {
-  is_loop: bool,
-  loop_index: uint,
-  loop_depth: uint,
-  block: @B
-}
-
-pub struct Allocator {
-  config: ~Config,
-  loop_index: uint
-}
-
-pub impl<I:Instruction, B:Block<I> > Allocator {
-  // Go through graph and create linear list of blocks
-  priv fn flatten(&self, root: @B) -> ~[@BlockWrapper<B>] {
-    let mut res: ~[@BlockWrapper<B>] = ~[];
-    let mut queue = ~[root];
-
-    while queue.len() > 0 {
-    }
-
-    return res;
-  }
-
-  priv fn run(&self, root: @B) {
-    let list = self.flatten(root);
-    assert!(list.len() >= 1);
-  }
-
-  fn run(root: @B, config: Config) {
-    let a = ~Allocator {
-      config: ~config,
-      loop_index: 0
+      children: ~[]
     };
-    a.run(root);
   }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,104 +1,21 @@
-use linearscan::Interval;
+use linearscan::GraphBuilder;
 mod linearscan;
-
-struct Block {
-  instructions: ~[@Instruction],
-  predecessors: ~[@Block],
-  successors: ~[@Block]
-}
-
-struct Instruction {
-  id: uint,
-  kind: Kind,
-  inputs: ~[@Interval],
-  output: @Interval
-}
 
 #[deriving(Eq)]
 enum Kind {
   Action0,
-  Action1,
-  Move
-}
-
-impl linearscan::Instruction for Instruction {
-  fn id(&self) -> uint {
-    return self.id;
-  }
-
-  fn output(&self) -> @Interval {
-    return self.output;
-  }
-
-  fn input_count(&self) -> uint {
-    return self.inputs.len();
-  }
-
-  fn input_at(&self, i: uint) -> @Interval {
-    return self.inputs[i];
-  }
-
-  fn is_move(&self) -> bool {
-    return self.kind == Move;
-  }
-}
-
-impl linearscan::Block<Instruction> for Block {
-  fn instruction_count(&self) -> uint {
-    return self.instructions.len();
-  }
-
-  fn instruction_at(&self, i: uint) -> @Instruction {
-    return self.instructions[i];
-  }
-
-  fn predecessor_count(&self) -> uint {
-    return self.predecessors.len();
-  }
-
-  fn predecessor_at(&self, i: uint) -> @Block {
-    return self.predecessors[i];
-  }
-
-  fn successor_count(&self) -> uint {
-    return self.successors.len();
-  }
-
-  fn successort_at(&self, i: uint) -> @Block {
-    return self.successors[i];
-  }
-
-  fn insert_move(&self, pos: uint, from: @Interval, to: @Interval) {
-    fail!(fmt!("Not implemented yet %? %? %?", pos, from, to));
-  }
-}
-
-impl Instruction {
-  fn new(id: uint, kind: Kind, inputs: ~[@Interval]) -> @Instruction {
-    return @Instruction {
-      id: id,
-      kind: kind,
-      inputs: inputs,
-      output: Interval::new()
-    };
-  }
+  Action1
 }
 
 #[test]
-fn allocation_test() {
-  let mut instr = ~[];
+fn one_block_graph() {
+  let mut g: ~GraphBuilder<Kind> = GraphBuilder::new();
 
-  let act0 = Instruction::new(0, Action0, ~[]);
-  instr.push(act0);
-  instr.push(Instruction::new(2, Action1, ~[act0.output]));
-
-  let mut root = @Block {
-    instructions: instr,
-    successors: ~[],
-    predecessors: ~[]
+  do g.block() |b| {
+    let v = b.add(Action0, ~[]);
+    b.add(Action1, ~[v]);
+    b.end();
   };
 
-  linearscan::Allocator::run(root, linearscan::Config {
-    register_count: 4
-  });
+  io::println(fmt!("%?", g));
 }
