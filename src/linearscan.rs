b9b7e1830a45890968bc502f041c4b1e6419243e
Fedor Indutny <fedor.indutny@gmail.com>
2013-05-07 18:00:43 +0400

    linearscan: improvements

--- src/linearscan.rs
+++ src/linearscan.rs
@@ -1,3 +1,5 @@
+use std::smallintmap::SmallIntMap;
+
 type BlockId = uint;
 type InstrId = uint;
 type IntervalId = uint;
@@ -8,26 +10,29 @@ pub struct GraphBuilder<K> {
   block_id: BlockId,
   instr_id: InstrId,
   interval_id: IntervalId,
-  blocks: ~[@mut Block<K>]
+  intervals: ~SmallIntMap<~Interval>,
+  blocks: ~SmallIntMap<~Block<K> >,
+  instructions: ~SmallIntMap<~Instruction<K> >
 }
 
 pub struct BlockBuilder<'self, K> {
   graph: &'self mut GraphBuilder<K>,
-  block: @mut Block<K>
+  block: &'self mut ~Block<K>
 }
 
 struct Block<K> {
   id: BlockId,
-  instructions: ~[@Instruction<K>],
-  successors: ~[@mut Block<K>],
-  predecessors: ~[@mut Block<K>]
+  instructions: ~[InstrId],
+  successors: ~[BlockId],
+  predecessors: ~[BlockId],
+  ended: bool
 }
 
 struct Instruction<K> {
   id: InstrId,
   kind: InstrKind<K>,
-  output: @Interval,
-  inputs: ~[@Interval]
+  output: IntervalId,
+  inputs: ~[IntervalId]
 }
 
 // Abstraction to allow having user-specified instruction types
@@ -41,8 +46,8 @@ struct Interval {
   id: IntervalId,
   value: Value,
   ranges: ~[LiveRange],
-  parent: Option<@Interval>,
-  children: ~[@Interval]
+  parent: Option<IntervalId>,
+  children: ~[IntervalId]
 }
 
 enum Value {
@@ -57,33 +62,54 @@ struct LiveRange {
 }
 
 pub impl<K> GraphBuilder<K> {
-  fn new() -> ~GraphBuilder<K> {
-    return ~GraphBuilder {
+  fn new() -> GraphBuilder<K> {
+    return GraphBuilder {
       block_id: 0,
       instr_id: 0,
       interval_id: 0,
-      blocks: ~[]
+      intervals: ~SmallIntMap::new(),
+      blocks: ~SmallIntMap::new(),
+      instructions: ~SmallIntMap::new()
     };
   }
 
-  fn block(&mut self, body: &fn(b: &mut BlockBuilder<K>)) {
-    let b = Block::new(self);
-    self.blocks.push(b);
-    BlockBuilder::exec(self, b, body);
+  fn block(&mut self, body: &fn(b: &mut BlockBuilder<K>)) -> BlockId {
+    let mut block = ~Block::new(self);
+    let id = block.id;
+    self.blocks.insert(id, block);
+
+    // Execute body
+    self.with_block(id, body);
+
+    return id;
   }
 
+  fn with_block(&mut self, id: BlockId, body: &fn(b: &mut BlockBuilder<K>)) {
+    let mut b = BlockBuilder {
+      graph: self,
+      block: self.blocks.find_mut(&id).unwrap()
+    };
+    body(&mut b);
+  }
+
+  fn verify(&mut self) {
+  }
+
+  #[inline(always)]
   priv fn block_id(&mut self) -> BlockId {
     let r = self.block_id;
     self.block_id += 1;
     return r;
   }
 
+  #[inline(always)]
   priv fn instr_id(&mut self) -> InstrId {
     let r = self.instr_id;
     self.instr_id += 2;
     return r;
   }
 
+  #[inline(always)]
   priv fn interval_id(&mut self) -> IntervalId {
     let r = self.interval_id;
     self.interval_id += 1;
@@ -92,59 +118,72 @@ pub impl<K> GraphBuilder<K> {
 }
 
 pub impl<'self, K> BlockBuilder<'self, K> {
-  priv fn exec(graph: &mut GraphBuilder<K>,
-               block: @mut Block<K>,
-               body: &fn(b: &mut BlockBuilder<K>)) {
-    let mut b = BlockBuilder {
-      graph: graph,
-      block: block
-    };
-    body(&mut b);
+  fn add(&mut self, kind: K, args: ~[InstrId]) -> InstrId {
+    assert!(!self.block.ended);
+    let id = Instruction::new(self.graph, User(kind), args);
+    self.block.instructions.push(id);
+    return id;
   }
 
-  fn add(&'self self, kind: K, args: ~[@Instruction<K>]) -> @Instruction<K> {
-    let r = Instruction::new(self.graph, User(kind), args);
-    self.block.instructions.push(r);
-    return r;
+  fn end(&mut self) {
+    assert!(!self.block.ended);
+    self.block.ended = true;
   }
 
-  fn end(&self) {
+  fn goto(&mut self, target_id: BlockId) {
+    assert!(self.block.successors.len() <= 2);
+    self.block.successors.push(target_id);
+
+    self.graph.blocks.find_mut(&target_id).unwrap().add_predecessor(self.block.id);
+    self.end();
   }
 }
 
 pub impl<K> Block<K> {
-  fn new(graph: &mut GraphBuilder<K>) -> @mut Block<K> {
-    return @mut Block {
+  fn new(graph: &mut GraphBuilder<K>) -> Block<K> {
+    return Block {
       id: graph.block_id(),
       instructions: ~[],
       successors: ~[],
-      predecessors: ~[]
+      predecessors: ~[],
+      ended: false
     };
+  }
+
+  fn add_predecessor(&mut self, pred: BlockId) {
+    assert!(self.predecessors.len() <= 2);
+
+    self.predecessors.push(pred);
   }
 }
 
 pub impl<K> Instruction<K> {
-  fn new(graph: &mut GraphBuilder<K>, kind: InstrKind<K>, args: ~[@Instruction<K>])
-      -> @Instruction<K> {
-    return @Instruction {
+  fn new(graph: &mut GraphBuilder<K>, kind: InstrKind<K>, args: ~[InstrId]) -> InstrId {
+    let r = Instruction {
       id: graph.instr_id(),
       kind: kind,
       output: Interval::new(graph),
-      inputs: do vec::map(args) |arg| {
-        arg.output
+      inputs: do vec::map(args) |id| {
+        graph.instructions.get(id).output
       }
     };
+    let id = r.id;
+    graph.instructions.insert(r.id, ~r);
+    return id;
   }
 }
 
 pub impl<K> Interval {
-  fn new(graph: &mut GraphBuilder<K>) -> @Interval {
-    return @Interval {
+  fn new(graph: &mut GraphBuilder<K>) -> IntervalId {
+    let r = Interval {
       id: graph.interval_id(),
       value: Virtual,
       ranges: ~[],
       parent: None,
       children: ~[]
     };
+    let id = r.id;
+    graph.intervals.insert(r.id, ~r);
+    return id;
   }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,21 +1,38 @@
+extern mod std;
 use linearscan::GraphBuilder;
 mod linearscan;
 
 #[deriving(Eq)]
 enum Kind {
   Action0,
-  Action1
+  Action1,
+  Goto
 }
 
 #[test]
 fn one_block_graph() {
-  let mut g: ~GraphBuilder<Kind> = GraphBuilder::new();
+  let mut g: ~GraphBuilder<Kind> = ~GraphBuilder::new();
 
   do g.block() |b| {
     let v = b.add(Action0, ~[]);
     b.add(Action1, ~[v]);
-    b.end();
+  };
+}
+
+#[test]
+fn loop_graph() {
+  let mut g: ~GraphBuilder<Kind> = ~GraphBuilder::new();
+
+  let b0 = do g.block() |b| {
+    b.add(Goto, ~[]);
   };
 
-  io::println(fmt!("%?", g));
+  let b1 = do g.block() |b| {
+    b.add(Goto, ~[]);
+    b.goto(b0);
+  };
+
+  do g.with_block(b0) |b| {
+    b.goto(b1);
+  }
 }
