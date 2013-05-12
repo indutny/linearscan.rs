3cf047910a89c08faeb20499c554c1c962ed463a
Fedor Indutny <fedor.indutny@gmail.com>
2013-05-12 13:50:45 +0400

    flatten: use BitvSet everywhere

--- src/linearscan/flatten.rs
+++ src/linearscan/flatten.rs
@@ -1,4 +1,5 @@
 use std::smallintmap::SmallIntMap;
+use std::bitv::BitvSet;
 use linearscan::graph::{Graph, BlockId};
 
 struct MapResult {
@@ -24,15 +25,15 @@ trait FlattenHelper {
 impl<K> FlattenHelper for Graph<K> {
   fn flatten_get_ends(&mut self) -> ~SmallIntMap<~[BlockId]> {
     let mut queue = ~[self.root];
-    let mut visited = ~SmallIntMap::new();
+    let mut visited = ~BitvSet::new();
     let mut ends: ~SmallIntMap<~[BlockId]> = ~SmallIntMap::new();
 
     // Visit each block and find loop ends
     while queue.len() > 0 {
       let cur = queue.shift();
-      visited.insert(cur, true);
+      visited.insert(cur);
       for self.get_block(cur).successors.each() |succ| {
-        if visited.contains_key(succ) {
+        if visited.contains(succ) {
           // Loop detected
           if ends.contains_key(succ) {
             ends.find_mut(succ).unwrap().push(cur);
@@ -53,7 +54,7 @@ impl<K> FlattenHelper for Graph<K> {
     let mut loop_index = 0;
 
     for ends.each() |start, ends| {
-      let mut visited = ~SmallIntMap::new();
+      let mut visited = ~BitvSet::new();
       let mut queue = ~[];
       let expected_depth = self.get_block(*start).loop_depth;
 
@@ -65,7 +66,7 @@ impl<K> FlattenHelper for Graph<K> {
 
         // Set depth and index of not-visited-yet nodes,
         // if we're not visiting nested loop
-        if block.loop_depth == expected_depth && visited.insert(cur, true) {
+        if block.loop_depth == expected_depth && visited.insert(cur) {
           block.loop_index = loop_index;
           block.loop_depth += 1;
         }
@@ -88,14 +89,14 @@ impl<K> Flatten for Graph<K> {
   fn flatten(&mut self) -> ~[BlockId] {
     let mut queue = ~[self.root];
     let mut result = ~[];
-    let mut visited = ~SmallIntMap::new();
+    let mut visited = ~BitvSet::new();
 
     // Visit each block and find loop ends
     while queue.len() > 0 {
       let cur = queue.shift();
 
       // Skip visited blocks
-      if visited.insert(cur, true) {
+      if visited.insert(cur) {
         result.push(cur);
 
         // Visit successors in loop order
